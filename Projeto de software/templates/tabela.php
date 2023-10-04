f6df04d2544a2189f2374dc51a897605eb91ea6f
Wenes11 <133669392+Wenes11@users.noreply.github.com>
2023-10-04 19:36:48 -0300

    Add files via upload

---
+++ Projeto de software/templates/tabela.php
@@ -0,0 +1,8 @@
+<?php
+ $times = array("Campeão Ingles", "Campeão Italiano", "Lider do Brasileiro", "Campeão espanhol");
+ $timesB = array("Rebaixados da premier legue","Rebaixados do italiano", "Zona de rebaixamento brasileirão","Rebaixados do Espanhol");
+ $conecte = new mysqli($times,$timesB);
+?>
+
+
+
